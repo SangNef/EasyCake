40b1b1d74630970f87c24342a9a722b5df58c6cb
SangNef <sang.tv2614@gmail.com>
2024-07-17 16:01:50 +0700

    full cake

--- upgrade.js
+++ upgrade.js
@@ -1,6 +1,4 @@
 function toggleModal(modalId) {
     const modal = document.getElementById(modalId);
-    const checkinModal = document.getElementById('checkin-modal');
     modal.classList.toggle('hidden');
-    checkinModal.classList.toggle('hidden');
   }
